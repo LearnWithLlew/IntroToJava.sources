fa9d9690235ad03c98ff8c8ee86198cc68d1a7c3
Llewellyn Falco <isidore@setgame.com>
2016-06-01 21:24:57 +0300

    mac paths

--- src/org/learnwithllew/teachingextensions/setup/tests/UnzipTest.java
+++ src/org/learnwithllew/teachingextensions/setup/tests/UnzipTest.java
@@ -2,6 +2,9 @@ package org.learnwithllew.teachingextensions.setup.tests;
 
 import java.io.File;
 
+import org.junit.Test;
+import org.learnwithllew.teachingextensions.setup.SetupConfig;
+
 import com.spun.util.io.ZipUtils;
 
 public class UnzipTest // extends TestCase
@@ -11,5 +14,10 @@ public class UnzipTest // extends TestCase
     String zip = "C:/Users/Llewellyn/Downloads/master/eclipse_workspace.zip";
     String dir = "C:/Users/Llewellyn/Downloads/master/";
     ZipUtils.doUnzip(new File(dir), new File(zip));
+  }
+  @Test
+  public void testEclipseForMacPath() throws Exception
+  {
+    System.out.println("Path=" + SetupConfig.getEclipsePathForMac());
   }
 }
