36ae97fe017832c0a66e1f73007e259ad6fdf46b
Llewellyn Falco <isidore@setgame.com>
2020-08-15 17:35:13 -0700

    t added square

--- src/test/org/learnwithllew/courseware/recipes/completed/SimpleSquare.java
+++ src/test/org/learnwithllew/courseware/recipes/completed/SimpleSquare.java
@@ -1,10 +1,16 @@
 package org.learnwithllew.courseware.recipes.completed;
 
+import org.junit.Test;
 import org.learnwithllew.teachingextensions.logo.Colors;
 import org.learnwithllew.teachingextensions.logo.Tortoise;
+import org.learnwithllew.teachingextensions.logo.utils.TortoiseUtils;
 
 public class SimpleSquare
 {
+  @Test
+  public void testCompleted() {
+    TortoiseUtils.verifyMain(this.getClass());
+  }
   public static void main(String[] args) throws Exception
   {
     //  Show the tortoise --#1
@@ -15,7 +21,7 @@ public class SimpleSquare
     for (int i = 1; i <= 4; i++)
     {
       //      Change the color of the line the tortoise draws to "blue" --#4
-      Tortoise.setPenColor(Colors.getRandomColor());
+      Tortoise.setPenColor(Colors.Blues.Blue);
       //      Move the tortoise 50 pixels --#2
       Tortoise.move(50);
       //      Turn the tortoise to the right (90 degrees) --#3
